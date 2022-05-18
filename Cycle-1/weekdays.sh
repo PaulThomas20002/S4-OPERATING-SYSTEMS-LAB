27f417725774011458567236d1f47ed22a67418d
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2022-05-18 12:17:26 +0530

    Create weekdays.sh

---
+++ Cycle-1/weekdays.sh
@@ -0,0 +1,20 @@
+echo "Enter the number"
+read day
+case $day in 
+1)
+echo "Mon";;
+2)
+echo "Tue";;
+3)
+echo "Wed";;
+4)
+echo "Thu";;
+5)
+echo "Fri";;
+6)
+echo "Sat";;
+7)
+echo "Sun";;
+*)
+echo "Unknown";;
+esac
