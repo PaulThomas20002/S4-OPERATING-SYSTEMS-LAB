9ea1c8b4bba39cd87e4ec021c28744ae0a3ce023
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2022-05-18 10:31:51 +0530

    first shell programmin code

---
+++ Cycle-1/operationofnum.sh
@@ -0,0 +1,10 @@
+echo "Hello"
+
+echo "Enter the number 1 :"
+read a
+echo "Enter the number 2 : "
+read b
+echo "a + b = " $((a+b))
+echo "a - b = " $((a-b))
+echo "a * b = " $((a*b))
+echo "a / b = " $((a/b))
