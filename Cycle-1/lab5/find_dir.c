11a15a721f3b6d96a6ae046af557cec7404b620c
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2022-06-21 11:11:27 +0530

    Update find_dir.c

--- Cycle-1/lab5/find_dir.c
+++ Cycle-1/lab5/find_dir.c
@@ -14,25 +14,28 @@ STEP 7: Close the directory.
 PROGRAM:*/
 
 
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<dirent.h>
+#include <dirent.h>
 #include <sys/stat.h>
+
 struct dirent *dptr;
-int main(int argc, char *argv[])
-{
-char buff[100];
-DIR *dirp;
-printf(“\n\n ENTER DIRECTORY NAME”);
-scanf(“%s”, buff);
-if((dirp=opendir(buff))==NULL)
-{
-printf(“The given directory does not exist”);
-exit(1);
-}
-while(dptr=readdir(dirp))
-{
-printf(“%s\n”,dptr->d_name);
-}
+
+int main (int argc, char *argv[]){
+        char buff[100];
+        struct stat stats;
+        DIR *dirp;
+        printf("Enter the directory name : ");
+        scanf("%s", buff);
+
+        if((dirp=opendir(buff)) == NULL){
+        printf("The given directory doesnot exist \n");
+        exit(1);
+        }
+
+        while(dptr = readdir(dirp)){
+        printf("%s\n", dptr->d_name);
+        }
 closedir(dirp);
 }
+
