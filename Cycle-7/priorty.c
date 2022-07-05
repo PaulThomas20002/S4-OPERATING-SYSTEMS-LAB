1db74497c8e4a6a4391b8d6131fe2bcb08894589
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2022-07-05 12:09:09 +0530

    Update priorty.c

--- Cycle-7/priorty.c
+++ Cycle-7/priorty.c
@@ -1,3 +1,23 @@
+/*
+
+
+Ex.No:5
+CPU SCHEDULING ALGORITHMS
+PRIORITY
+AIM:
+To write a C program for implementation of Priority scheduling algorithms.
+ALGORITHM:
+Step 1: Inside the structure declare the variables.
+Step 2: Declare the variable i,j as integer, totwtime and totttime is equal to zero.
+Step 3: Get the value of „n‟ assign p and allocate the memory.
+Step 4: Inside the for loop get the value of burst time and priority.
+Step 5: Assign wtime as zero .
+Step 6: Check p[i].pri is greater than p[j].pri .
+Step 7: Calculate the total of burst time and waiting time and assign as turnaround time.
+Step 8: Stop the program
+
+*/
+
 //PROGRAM TO STIMULATE NON-PREEMPTIVE SCHEDULING ALGORITHMS
 //PROGRAMS :
 //1)PRIORITY
